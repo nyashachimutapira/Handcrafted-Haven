3167c3d2578672b18e6c6d19b105b0c7543876d7
nyashachimutapira <chimutapiran@gmail.com>
2026-02-02 12:04:18 +0200

    commit

--- src/components/cart/CheckoutForm.tsx
+++ src/components/cart/CheckoutForm.tsx
@@ -279,7 +279,7 @@ export default function CheckoutForm({ cartTotal, itemCount }: CheckoutFormProps
                 required
                 className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                 placeholder="MM/YY"
-                maxLength="5"
+                maxLength={5}
               />
             </div>
 
@@ -293,7 +293,7 @@ export default function CheckoutForm({ cartTotal, itemCount }: CheckoutFormProps
                 required
                 className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
                 placeholder="123"
-                maxLength="4"
+                maxLength={4}
               />
             </div>
           </div>
